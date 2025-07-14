fe4c9372cbbcc9d0ef1f709c33d7ab1fae5a004c
Queentel <joiqueentel@gmail.com>
2025-07-14 13:39:27 +0300

    react hooks

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data"; // ✅ updated from user.js → data.js
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I love creating awesome user interfaces with React!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
+
+
